3536b27633dc901ebfc480114f05f86819d35c72
diego <diego.vincent.rodriguez@gmail.com>
2023-03-18 02:28:01 -0700

    chore: it's cleaner

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,5 @@
 <h1>Diego Rodriguez</h1>
 
-<hr />
-
 <ul>
 	<li><a href="https://twitter.com/asciidiego" target="_blank">Twitter</a></li>
 	<li><a href="http://linkedin.com/in/asciidiego" target="_blank">LinkedIn</a></li>
