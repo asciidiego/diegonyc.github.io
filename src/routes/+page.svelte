6fd1c18b9a1b51ecfda40db5d76bdb7145b94f89
diego <diego.vincent.rodriguez@gmail.com>
2023-03-18 03:48:47 -0700

    intro

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,51 @@
-<h1>Diego Rodriguez</h1>
+<script>
+	export let data;
+</script>
 
-<ul>
-	<li><a href="https://twitter.com/asciidiego" target="_blank">Twitter</a></li>
-	<li><a href="http://linkedin.com/in/asciidiego" target="_blank">LinkedIn</a></li>
-</ul>
+<div class="container">
+	<header>
+		<h1>Diego Rodríguez</h1>
+	</header>
+
+	<main>
+		<h2>So, what do you do?</h2>
+
+		<p>
+			In 1987, Apple Computer, Inc. published the following quote in “Human Interface Guidelines:
+			The Apple Desktop Interface”:
+		</p>
+		<q> The human interface is the sum of all communication between human and computer</q>.
+		<p>
+			Given Apple's prevalence in today's world, we can say that—to a significant degree—yes, the
+			human interface <i>is</i> the sum of all communication between humans and computers. After
+			all, the iPhone <i>is</i> a computer.
+		</p>
+		<p>
+			We are not in 1987 anymore. It's {data.year} and personal computers and mobile devices are everywhere.
+		</p>
+		<p>Yet... human interfaces have barely changed.</p>
+
+		<p>And that's good.</p>
+
+		<p>
+			A great predictor of optimality is time resistance; sometimes, if something works for a long
+			time, it will (probably) continue to do so. User interfaces are no exception.
+		</p>
+
+		<p>But.</p>
+
+		<p>
+			Where is the interface that sums all communication between natural and artificial
+			intelligence?
+		</p>
+
+		<p>That's what I do.</p>
+	</main>
+
+	<aside>
+		<ul>
+			<li><a href="https://twitter.com/asciidiego" target="_blank">Twitter</a></li>
+			<li><a href="http://linkedin.com/in/asciidiego" target="_blank">LinkedIn</a></li>
+		</ul>
+	</aside>
+</div>
