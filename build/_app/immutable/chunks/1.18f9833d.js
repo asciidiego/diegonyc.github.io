f5e7f43f5c4128c7d1cf42c6dd1aa17e12850bf1
diego <diego.vincent.rodriguez@gmail.com>
2023-03-18 02:32:11 -0700

    fuck em

--- build/_app/immutable/chunks/1.18f9833d.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/error.svelte.76d7fa83.js";export{t as component};
--- build/_app/immutable/chunks/2.3ad05e42.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../entry/_page.svelte.bc34c647.js";export{t as component};
