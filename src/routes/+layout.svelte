bbb90d3d184780b879847a5fced4c5f00dcaceb7
diego <d@krea.ai>
2023-09-14 19:32:51 -0700

    declutter

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -42,65 +42,19 @@
 	];
 </script>
 
-<div class="root">
-	<aside class="left-menu">
-		<h1>Diego Rodriguez</h1>
-		<ul>
-			{#each menuItems as item, index (item.id ?? index)}
-				{#if item.type === 'link'}
-					<li>
-						{#if $page.route.id === item.id}
-							<span class="underline">{item.title}</span>
-						{:else}
-							<a href={item.id}>{item.title}</a>
-						{/if}
-					</li>
-				{:else if item.type === 'divider'}
-					<li style="width: 16px"><hr /></li>
-				{/if}
-			{/each}
-		</ul>
-	</aside>
-
+<main class="container">
 	<slot />
-</div>
+</main>
 
 <style>
-	/* Make horizontal */
-	ul {
-		list-style: none;
-		padding: 0;
-		/* display: flex; */
-		justify-content: center;
-		gap: 1em;
-	}
-
-	li {
-		font-size: 1.2em;
-		font-weight: bold;
-	}
-
-	li a {
-		color: #000;
-		text-decoration: none;
-	}
-
-	li a:hover {
-		text-decoration: underline;
-	}
-
-	.underline {
-		text-decoration: underline;
-	}
-
-	.root {
-		display: flex;
-		width: 100%;
-		height: 100%;
-	}
-
-	.left-menu {
-		width: 128px;
-		padding: 0 2em 0em 0em;
+	.container {
+		@media (max-width: 640px) {
+			/* phones */
+			padding: 48px 36px;
+		}
+		@media (min-width: 640px) {
+			/* tablet+ */
+			padding: 96px 72px;
+		}
 	}
 </style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,59 +3,83 @@
 </script>
 
 <div class="container">
+	<header>
+		<h1 class="title">Diego Rodriguez</h1>
+	</header>
+
 	<main>
-		<h2>So, what do you do?</h2>
+		<p>About me:</p>
+		<ul class="list">
+			<li>I <span style="font-style: italic;">live</span> in San Francisco, California.</li>
+			<ul>
+				<li>
+					Before that, I lived in...
+					<ul>
+						<li>Miami;</li>
+						<li>New York City;</li>
+						<li>Barcelona;</li>
+						<li>and Maracaibo.</li>
+						<li style="font-style: italic;">In reverse order.</li>
+					</ul>
+				</li>
+			</ul>
+
+			<li>
+				I <span style="font-style: italic;">enjoy</span> writing, reading, programming, meditation, archery,
+				and listening to people and making them laugh.
+			</li>
+
+			<li>
+				I <span style="font-style: italic;">work</span> at
+				<a href="krea.ai" target="_blank">KREA</a>. A design tool with AI inside.
+			</li>
+
+			<li>
+				I <span style="font-style: italic;">wonder</span> about the role of simplicity in my life.
+			</li>
+		</ul>
+
+		<hr />
+
+		<p>As you may have guessed by the name of this page, I like computers.</p>
 
 		<p>
-			In 1987, Apple Computer, Inc. published the following quote in “Human Interface Guidelines:
-			The Apple Desktop Interface”:
+			Like humans, they can be fun (and boring), powerful, (and weak), and pleasing (and
+			frustrating).
 		</p>
-		<q> The human interface is the sum of all communication between human and computer</q>.
-		<p>
-			Given Apple's prevalence in today's world, we can say that—to a significant degree—yes, the
-			human interface <i>is</i> the sum of all communication between humans and computers. After
-			all, the iPhone <i>is</i> a computer.
-		</p>
-		<p>
-			We are not in 1987 anymore. It's {data.year} and personal computers and mobile devices are everywhere.
-		</p>
-		<p>Yet... human interfaces have barely changed.</p>
 
-		<p>And that's good.</p>
+		<p>Can they be creative and loving though?</p>
+
+		<p>I don't know yet. But we'll find out soon.</p>
+
+		<hr />
 
 		<p>
-			A great predictor of optimality is time resistance; if something works for a long time, it
-			will—probably—continue to do so. User interfaces are no exception.
+			<span>If you also enjoy working with computers, send me an email to </span>
+			<a href="mailto:d@krea.ai">d@krea.ai</a><span>.</span>
 		</p>
 
-		<p>But.</p>
-
-		<p>
-			Where is the interface that sums all communication between natural and artificial
-			intelligence?
-		</p>
-
-		<p>That's what I do.</p>
-
-		<hr class="divider" />
-
-		<p>
-			I currently live in California and work at
-			<a href="https://www.twitter.com/krea_ai" target="_blank">KREA</a>.
-		</p>
+		You<span style="font-style: italic;">can</span> find me on Twitter; I'm still exploring the role
+		of social media in my life.
 	</main>
 </div>
 
 <style>
+	.title {
+		font-size: x-large;
+		margin: 0;
+		font-family: Ivar Display Hydro;
+	}
+	.list li {
+		margin: 8px 0;
+	}
+
 	.container {
 		display: grid;
 		width: 100%;
-		place-items: center;
 	}
 
-	.divider {
-		height: 1px;
-		background-color: #00000022;
-		border: none;
+	* {
+		font-family: 'Ivar Hydro', 'Times New Roman';
 	}
 </style>
